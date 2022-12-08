366554adcc5a83de226cecffb4b5c1676bb9da35
mehdy28 <118557115+mehdy28@users.noreply.github.com>
2022-12-08 20:23:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,39 @@ app.layout = html.Div([
         figure=fig
     )
 ])
-
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics",),
+        html.P(
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
 if __name__ == '__main__':
     app.run_server(debug=True)
